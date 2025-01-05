70d8c3987ded0571f0109310bfa7d1451e4b2df7
Gen Gamer <privatehirakbhai35@gmail.com>
2025-01-05 08:43:05 +0530

    Update SEO.tsx

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -14,12 +14,8 @@ const SEO = ({ title, description, canonicalUrl, socialMeta, robots = "max-image
       {/* Open Graph / Social Meta */}
       {socialMeta && (
         <>
-          <meta property="og:type" content={socialMeta.type || "website"} />
           <meta property="og:url" content={socialMeta.url} />
-          <meta property="og:title" content={socialMeta.title} />
-          <meta property="og:description" content={socialMeta.description} />
           <meta property="og:image" content={socialMeta.image} />
-          <meta property="og:site_name" content="SWP Calculator" />
         </>
       )}
     </Helmet>
