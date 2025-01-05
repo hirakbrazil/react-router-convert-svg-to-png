6ce141dd3d2c69143df0f07214f4aaf164949f5b
Gen Gamer <privatehirakbhai35@gmail.com>
2025-01-05 07:51:56 +0530

    Create SEO.tsx

---
+++ src/components/SEO.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Helmet } from "react-helmet";
+
+const SEO = ({ title, description, canonicalUrl, socialMeta, robots = "max-image-preview:large" }) => {
+  return (
+    <Helmet>
+      <title>{title}</title>
+      <meta name="description" content={description} />
+      <meta name="robots" content={robots} />
+
+      {/* Canonical URL */}
+      {canonicalUrl && <link rel="canonical" href={canonicalUrl} />}
+
+      {/* Open Graph / Social Meta */}
+      {socialMeta && (
+        <>
+          <meta property="og:type" content={socialMeta.type || "website"} />
+          <meta property="og:url" content={socialMeta.url} />
+          <meta property="og:title" content={socialMeta.title} />
+          <meta property="og:description" content={socialMeta.description} />
+          <meta property="og:image" content={socialMeta.image} />
+          <meta property="og:site_name" content={socialMeta.siteName} />
+        </>
+      )}
+    </Helmet>
+  );
+};
+
+export default SEO;
