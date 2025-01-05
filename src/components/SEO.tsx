d71f2b01163a6c50ad4d52dac237923a413d6f00
Gen Gamer <privatehirakbhai35@gmail.com>
2025-01-05 12:51:28 +0530

    Update SEO.tsx

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -2,47 +2,46 @@ import React from 'react';
 import { Helmet } from 'react-helmet';
 
 interface SEOProps {
-  title: string;
-  description: string;
-  canonicalUrl: string;
+  title?: string;
+  description?: string;
+  canonicalUrl?: string;
   robots?: string;
   ogTitle?: string;
   ogDescription?: string;
   ogUrl?: string;
   ogImage?: string;
-  ogType?: 'website' | 'article';  // Support for dynamic og:type
+  ogType?: 'website' | 'article'; // Support for dynamic og:type
 }
 
 const SEO: React.FC<SEOProps> = ({
   title,
   description,
   canonicalUrl,
-  robots = "max-image-preview:large",
+  robots,
   ogTitle,
   ogDescription,
   ogUrl,
   ogImage,
-  ogType = 'website',  // Default to 'website' for most pages
+  ogType,
 }) => {
   return (
     <Helmet>
-      {/* Title and Description */}
-      <title>{title}</title>
-      <meta name="description" content={description} />
-      
-      {/* Canonical Link */}
-      <link rel="canonical" href={canonicalUrl} />
+  
+      {title && <title>{title}</title>}
 
-      {/* Robots meta tag */}
-      <meta name="robots" content={robots} />
-      
-      {/* Open Graph */}
-      <meta property="og:type" content={ogType} />  {/* Dynamic og:type */}
-      <meta property="og:url" content={ogUrl || canonicalUrl} />
-      <meta property="og:title" content={ogTitle || title} />
-      <meta property="og:description" content={ogDescription || description} />
-      <meta property="og:image" content={ogImage || "https://swp-calculator.mutualfundjournal.in/banner.jpg"} />
-      <meta property="og:site_name" content="SWP Calculator" />
+
+      {description && <meta name="description" content={description} />}
+
+      {canonicalUrl && <link rel="canonical" href={canonicalUrl} />}
+
+  
+      {robots && <meta name="robots" content={robots} />}
+
+      {ogType && <meta property="og:type" content={ogType} />}
+      {ogUrl && <meta property="og:url" content={ogUrl} />}
+      {ogTitle && <meta property="og:title" content={ogTitle} />}
+      {ogDescription && <meta property="og:description" content={ogDescription} />}
+      {ogImage && <meta property="og:image" content={ogImage} />}
     </Helmet>
   );
 };
