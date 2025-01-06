42bee83a229ea7fa66585df04687ecbf1c56a972
Gen Gamer <privatehirakbhai35@gmail.com>
2025-01-06 11:28:38 +0530

    Update ShareDialog.tsx

--- src/components/ShareDialog.tsx
+++ src/components/ShareDialog.tsx
@@ -52,7 +52,7 @@ const ShareDialog = ({
     navigator.clipboard.writeText(generateShareableLink(false));
     toast({
       title: "Link copied to clipboard",
-      duration: 4000,
+      duration: 5000,
     });
   };
 
