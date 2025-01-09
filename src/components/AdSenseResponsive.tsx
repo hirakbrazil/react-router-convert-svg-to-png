3f9e860d1dfedb12e4e369429a2b6aa5f1b1eb78
Gen Gamer <privatehirakbhai35@gmail.com>
2025-01-09 11:41:35 +0530

    Create AdSenseResponsive.tsx

---
+++ src/components/AdSenseResponsive.tsx
@@ -0,0 +1,22 @@
+import React, { useEffect } from 'react';
+
+const AdSenseResponsive: React.FC = () => {
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      (window.adsbygoogle = window.adsbygoogle || []).push({});
+    }
+  }, []);
+
+  return (
+    <ins
+      className="adsbygoogle"
+      style={{ display: 'block' }}
+      data-ad-client="ca-pub-5161705954627943"
+      data-ad-slot="6591128075"
+      data-ad-format="auto"
+      data-full-width-responsive="true"
+    ></ins>
+  );
+};
+
+export default AdSenseResponsive;
