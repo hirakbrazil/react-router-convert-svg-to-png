3ac76bdbd9884c2a7f00d59964e3fda939128fb5
Gen Gamer <privatehirakbhai35@gmail.com>
2025-01-09 11:55:33 +0530

    Update Feedback.tsx

--- src/pages/Feedback.tsx
+++ src/pages/Feedback.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Footer from "@/components/Footer";
 import useTheme from "@/hooks/useTheme";
 import SEO from "@/components/SEO";
-import AdSenseResponsive from "@/components/AdSenseResponsive";
+import AdSenseHorizontal from "@/components/AdSenseHorizontal";
 
 
 const Feedback = () => {
@@ -23,7 +23,7 @@ const Feedback = () => {
       />
     <div className="min-h-screen bg-background text-foreground py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-3xl mx-auto space-y-8">
-        <AdSenseResponsive />
+        <AdSenseHorizontal />
         <div className="text-center">
           <h1 className="text-3xl font-bold text-foreground sm:text-4xl">
             Feedback
