3c9c9bdfca4cd006d7cb18bf63581ef90b206fed
Gen Gamer <privatehirakbhai35@gmail.com>
2025-01-09 20:08:13 +0530

    Update WithdrawalFrequencySelector.tsx

--- src/components/calculator/WithdrawalFrequencySelector.tsx
+++ src/components/calculator/WithdrawalFrequencySelector.tsx
@@ -24,13 +24,9 @@ const WithdrawalFrequencySelector = ({
 
   return (
     <div className="flex items-center justify-between">
-      <label className="text-lg text-gray-700 dark:text-[#c1cbd6] flex items-center">
-  Withdrawal frequency
-  <span className="ml-1 flex items-center">
-    <InfoTooltip 
-      content="If you want to receive payments or withdrawals every month, like a job or pension, select Monthly. Quarterly means receiving payments once every 3 months. Half-yearly means receiving 2 withdrawals in a year, or one every 6 months. Yearly/Annually means receiving 1 withdrawal in a year." 
-    />
-  </span>
+      <label>
+  <span className="text-lg text-gray-700 dark:text-[#c1cbd6]">Withdrawal frequenc</span>
+  <InfoTooltip content="If you want to receive payments or withdrawals every month, like a job or pension, select Monthly. Quarterly means receiving payments once every 3 months. Half-yearly means receiving 2 withdrawals in a year, or one every 6 months. Yearly/Annually means receiving 1 withdrawal in a year." />
 </label>
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
