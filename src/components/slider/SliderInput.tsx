e5b1e1a3916144cb1fc2db38be20b475392834a9
ff-apk <nitinadxui@gmail.com>
2025-01-24 12:18:10 +0530

    Update SliderInput.tsx

--- src/components/slider/SliderInput.tsx
+++ src/components/slider/SliderInput.tsx
@@ -93,7 +93,7 @@ const SliderInput = ({
         maxWidth: '300px',
       };
     } else {
-      const baseWidth = Math.max(210, inputValue.length * 14); // Increase baseWidth for better desktop handling
+      const baseWidth = Math.max(80, inputValue.length * 14); // Increase baseWidth for better desktop handling
     return {
       width: `${baseWidth}px`,
       minWidth: '210px', // Adjusted minWidth
