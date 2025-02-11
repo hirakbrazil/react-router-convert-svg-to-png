47b3568116f83e5997a3f6c709ba97989c90ca32
build-gg <94852783+build-gg@users.noreply.github.com>
2025-02-11 20:15:28 +0530

    Update ClipboardImage.tsx

--- src/components/ClipboardImage.tsx
+++ src/components/ClipboardImage.tsx
@@ -67,7 +67,7 @@ const ClipboardImage = () => {
           <div className="space-y-2">
             <div
               className={`flex justify-center transition-all duration-500 ${
-                isFormatSelectOpen ? "mb-22" : "mb-0"
+                isFormatSelectOpen ? "mb-24" : "mb-0"
               }`}
             >
               <Select 
