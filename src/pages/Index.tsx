74b965b07c63836b342922540cc981ebaf365b33
build-gg <94852783+build-gg@users.noreply.github.com>
2025-05-15 19:46:58 +0530

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -3,7 +3,6 @@ import React from "react";
 import Footer from "@/components/Footer";
 import SEO from "@/components/SEO";
 import CalculatorHeader from "@/components/calculator/CalculatorHeader";
-import AdSenseHorizontal from "@/components/AdSenseHorizontal";
 import AdSenseResponsive from "@/components/AdSenseResponsive";
 import DesktopSidebar from "@/components/DesktopSidebar";
 import ClipboardImage from "@/components/ClipboardImage";
@@ -15,17 +14,17 @@ const Index = () => {
       <SEO
         title="Clipboard to Image - Paste Image to Download"
         description="Easily download copied clipboard images with our free Clipboard to Image tool. Simply Paste Image to Download!"
-        canonicalUrl="https://clipboard-to-image.toolyoulove.com/"
+        canonicalUrl="https://pasteimagetodownload.com/"
         robots="max-image-preview:large"
         ogTitle="Clipboard to Image - Paste Image to Download"
         ogDescription="Easily download copied clipboard images with our free Clipboard to Image tool. Simply Paste Image to Download!"
-        ogUrl="https://clipboard-to-image.toolyoulove.com/"
-        ogImage="https://clipboard-to-image.toolyoulove.com/banner.jpg"
+        ogUrl="https://pasteimagetodownload.com/"
+        ogImage="https://pasteimagetodownload.com/banner.jpg"
         ogType="website"
       />
       <div className="min-h-screen bg-background text-foreground py-12 px-4 sm:px-6 lg:px-8">
         <div className="max-w-3xl mx-auto space-y-8 lg:mr-[300px]">
-          <AdSenseHorizontal />
+          <AdSenseResponsive />
           <CalculatorHeader />
           <div className="border border-border bg-card dark:bg-card rounded-xl p-6">
             <ClipboardImage />
