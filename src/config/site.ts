b1d1fb0206e2c2b950c26af8f0a5feb76f3c00e5
build-gg <94852783+build-gg@users.noreply.github.com>
2025-06-26 11:08:13 +0530

    Update site.ts

--- src/config/site.ts
+++ src/config/site.ts
@@ -2,7 +2,4 @@
 export const siteConfig = {
   baseUrl: 'https://pasteimagetodownload.com',
   name: 'Paste Image to Download',
-  description: 'Easily download copied images with Paste Image to Download tool.',
-  email: 'info@pasteimagetodownload.com',
-  bannerImage: '/banner.jpg',
 };
