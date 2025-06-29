d047e8c8f5b93c35db463cb18e284af11d087b89
build-gg <94852783+build-gg@users.noreply.github.com>
2025-06-29 12:19:26 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { registerSW } from 'virtual:pwa-register'
 // Register service worker
 const updateSW = registerSW({
   onNeedRefresh() {
-  console.log('New version is available');
+    console.log('New version is available');
   },
   onOfflineReady() {
     console.log('App ready to work offline')
