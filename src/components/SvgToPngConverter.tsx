fc30c7a5bcae37b28b8ddd042d2d9536e0dbcdfa
hirakbrazil <hirakbrazil@gmail.com>
2025-07-07 13:06:23 +0530

    Update SvgToPngConverter.tsx added w-auto to selection menu

--- src/components/SvgToPngConverter.tsx
+++ src/components/SvgToPngConverter.tsx
@@ -156,7 +156,7 @@ const SvgToPngConverter = () => {
                         onValueChange={handleQualityChange}
                         disabled={isConverting}
                       >
-                        <SelectTrigger>
+                        <SelectTrigger className="w-auto">
                           <SelectValue />
                         </SelectTrigger>
                         <SelectContent>
