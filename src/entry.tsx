256eda97c94338e9776c5c99807e4a012cde7c3d
hirakbrazil <hirakbrazil@gmail.com>
2025-07-08 10:30:04 +0530

    Update entry.tsx

--- src/entry.tsx
+++ src/entry.tsx
@@ -1,16 +1,10 @@
+// entry.tsx
+import { ViteReactSSG } from 'vite-react-ssg'
+import App from './App'
+import { routes } from './routes'
 
-import React from 'react';
-import { renderToString } from 'react-dom/server';
-import App from './App';
-import { routes } from './routes';
-
-// This is the SSG entry point
-export default App;
-
-export { routes };
-
-// Export the render function for SSG
-export const render = (url: string) => {
-  // For SSG, we don't need the full router setup since each page is pre-rendered
-  return renderToString(React.createElement(App));
-};
+// Export createApp as required by vite-react-ssg
+export const createApp = ViteReactSSG({
+  App,
+  routes,
+})
