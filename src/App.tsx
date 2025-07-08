6d4f704bfc8dda3e1a17883c46efa0f3a74cf8b2
hirakbrazil <hirakbrazil@gmail.com>
2025-07-08 10:57:52 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,8 @@ import { Toaster as Sonner } from '@/components/ui/sonner'
 import { TooltipProvider } from '@/components/ui/tooltip'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { BrowserRouter, useRoutes } from 'react-router-dom'
-import { StaticRouter } from 'react-router' // ✅ correct import here
+// Change this import:
+import { StaticRouter } from 'react-router-dom/server'  // ✅ correct import here
 import ScrollToTop from '@/components/ScrollToTop'
 import { routes } from './routes'
 
