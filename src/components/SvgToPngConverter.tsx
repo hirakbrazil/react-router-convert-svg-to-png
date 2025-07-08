17be5ed6ab43d379c906e89020c2b7b94b655122
hirakbrazil <hirakbrazil@gmail.com>
2025-07-08 11:49:21 +0530

    Update SvgToPngConverter.tsx

--- src/components/SvgToPngConverter.tsx
+++ src/components/SvgToPngConverter.tsx
@@ -156,7 +156,7 @@ const SvgToPngConverter = () => {
                         onValueChange={handleQualityChange}
                         disabled={isConverting}
                       >
-                        <SelectTrigger className="w-[180px]">
+                        <SelectTrigger className="w-full sm:w-[180px]">
                           <SelectValue />
                         </SelectTrigger>
                         <SelectContent>
@@ -221,15 +221,6 @@ const SvgToPngConverter = () => {
                 </div>
               </CardContent>
             </Card>
-          )}
-
-          {/* Processing Status */}
-          {isConverting && (
-            <div className="text-center">
-              <p className="text-sm text-muted-foreground">
-                Converting {processedSvgs.length > 1 ? 'SVGs' : 'SVG'} to PNG...
-              </p>
-            </div>
           )}
 
           {/* SVG Items */}
