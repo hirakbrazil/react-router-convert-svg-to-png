ae186d362ad85ba0e61ade585e903ae32e49d7cb
hirakbrazil <hirakbrazil@gmail.com>
2025-07-08 10:51:11 +0530

    Update entry.tsx

--- src/entry.tsx
+++ src/entry.tsx
@@ -3,7 +3,6 @@ import { ViteReactSSG } from 'vite-react-ssg'
 import App from './App'
 import { routes } from './routes'
 
-// Export createApp as required by vite-react-ssg
 export const createApp = ViteReactSSG({
   App,
   routes,
