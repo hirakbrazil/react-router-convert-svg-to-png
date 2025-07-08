027d0b63eb99469a37c3dd9f1e7d0a321aee2e01
hirakbrazil <hirakbrazil@gmail.com>
2025-07-08 10:51:40 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,30 @@
-
 import { Toaster } from '@/components/ui/toaster'
 import { Toaster as Sonner } from '@/components/ui/sonner'
 import { TooltipProvider } from '@/components/ui/tooltip'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { BrowserRouter, useRoutes } from 'react-router-dom'
+import { BrowserRouter, StaticRouter, useRoutes } from 'react-router-dom'
 import ScrollToTop from '@/components/ScrollToTop'
 import { routes } from './routes'
 
 const queryClient = new QueryClient()
 
-const AppRoutes = () => {
-  const element = useRoutes(routes)
-  return element
-}
+const AppRoutes = () => useRoutes(routes)
 
-const App = () => {
+const App = ({ url }: { url?: string }) => {
+  const isSSR = typeof window === 'undefined'
+
+  const Router = isSSR ? StaticRouter : BrowserRouter
+  const routerProps = isSSR ? { location: url || '/' } : {}
+
   return (
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        {typeof window !== 'undefined' ? (
-          <BrowserRouter>
-            <ScrollToTop />
-            <AppRoutes />
-          </BrowserRouter>
-        ) : (
+        <Router {...routerProps}>
+          <ScrollToTop />
           <AppRoutes />
-        )}
+        </Router>
       </TooltipProvider>
     </QueryClientProvider>
   )
