cd4b0c3e65ad1f2716399986c8e2f2949895c59f
hirakbrazil <hirakbrazil@gmail.com>
2025-07-08 11:17:38 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { hydrateRoot } from 'react-dom/client'
+import { createRoot, hydrateRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
