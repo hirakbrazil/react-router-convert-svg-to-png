7ba7c5d2ad2edff36b331dfe083d0769771f41b4
hirakbrazil <hirakbrazil@gmail.com>
2025-07-09 12:33:02 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,12 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { toast } from 'sonner'
-import { register } from './sw/register';
 
 createRoot(document.getElementById("root")!).render(<App />);
 
-register().then((registerSW) => {
-  if (typeof registerSW === 'function') {
+if (import.meta.env.VITE_OFFLINE_SUPPORT === 'true') {
+  (async () => {
+    const { registerSW } = await import('virtual:pwa-register');
 // Register service worker
 const updateSW = registerSW({
   onNeedRefresh() {
@@ -46,5 +46,5 @@ setTimeout(() => {
     localStorage.removeItem('app-updated');
   }
 }, 200);
+})();
 }
-});
