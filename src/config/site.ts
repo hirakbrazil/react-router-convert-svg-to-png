9558c1652828137e7d4b91277b70aa95cbfd7083
hirakbrazil <hirakbrazil@gmail.com>
2025-07-09 10:59:43 +0530

    Update site.ts

--- src/config/site.ts
+++ src/config/site.ts
@@ -5,7 +5,7 @@ export const siteConfig = {
   description: 'Convert high-quality SVG files to PNG images instantly. Resize, optimize, and download your images effortlessly.',
   adsense: {
     pubId: 'ca-pub-3677847561110212',
-    adSlot: 'xxxxxxxxxx'
+    adSlot: '7495897394'
   },
   analytics: {
     ga4MeasurementId: 'G-RG2EBW5W0Y'
