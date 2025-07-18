34216ccc91b1e0218d5964c71658a55dc8066513
hirakbrazil <hirakbrazil@gmail.com>
2025-07-18 19:20:36 +0530

    Update root.tsx

--- app/root.tsx
+++ app/root.tsx
@@ -25,7 +25,7 @@ export const links: Route.LinksFunction = () => [
   },
   {
     rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
+    href: "https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap",
   },
   { rel: "icon", href: "/icon.png" },
   { rel: "apple-touch-icon", href: "/icon.png" },
