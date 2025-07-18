4f20d946ddc0d466183791e0629e7fe742246216
hirakbrazil <hirakbrazil@gmail.com>
2025-07-18 11:26:56 +0530

    Update routes.ts

--- app/routes.ts
+++ app/routes.ts
@@ -2,7 +2,7 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
   index("routes/Index.tsx"),
-  route("about", "routes/About.tsx"),
+  route("about", "routes/about.tsx"),
   route("feedback", "routes/Feedback.tsx"),
   route("privacy", "routes/Privacy.tsx"),
   route("*", "routes/404.tsx"),
