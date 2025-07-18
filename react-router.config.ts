306cba026c0d5596dd7e1e1440fded7144f02268
hirakbrazil <hirakbrazil@gmail.com>
2025-07-18 14:47:24 +0530

    Update react-router.config.ts

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,8 +1,7 @@
 import type { Config } from "@react-router/dev/config";
 
 export default {
-  // return a list of URLs to prerender at build time
-  async prerender() {
-    return ["/", "/about", "/feedback"];
-  },
+  // Config options...
+  // Server-side render by default, to enable SPA mode set this to `false`
+  ssr: false,
 } satisfies Config;
