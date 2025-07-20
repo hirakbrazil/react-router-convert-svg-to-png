46f956cec57910005c42907b43e90e467e46155c
hirakbrazil <hirakbrazil@gmail.com>
2025-07-20 20:23:36 +0530

    Update home.tsx

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import type { Route } from "./+types/home";
 import Footer from "@/components/Footer";
-import HeaderContent from "@/components/HeaderContent";
+import Header from "@/components/Header";
 import AdSenseResponsive from "@/components/AdSenseResponsive";
 import DesktopSidebar from "@/components/DesktopSidebar";
 import SvgToPngConverter from "@/components/SvgToPngConverter";
