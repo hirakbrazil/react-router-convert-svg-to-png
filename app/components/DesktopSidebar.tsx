927040c784ca9756095ac1a336d330741aa05342
hirakbrazil <hirakbrazil@gmail.com>
2025-07-20 20:47:01 +0530

    Update DesktopSidebar.tsx

--- app/components/DesktopSidebar.tsx
+++ app/components/DesktopSidebar.tsx
@@ -19,11 +19,11 @@ const DesktopSidebar = () => {
   if (!isLgUp) return null; // Don't render anything on < lg screens
 
   return (
-    <div className="hidden lg:block w-[300px] fixed right-0 top-0 h-screen p-4 bg-background">
+    <aside className="hidden lg:block w-[300px] fixed right-0 top-0 h-screen p-4 bg-background">
       <div className="sticky top-4">
         <AdSenseResponsive />
       </div>
-    </div>
+    </aside>
   );
 };
 
