e5ff93665c92ece18f1b7bd4f33ff7c242e5fff5
hirakbrazil <hirakbrazil@gmail.com>
2025-07-21 09:30:14 +0530

    Update routes.ts

--- app/routes.ts
+++ app/routes.ts
@@ -5,6 +5,7 @@ export default [
   route("about", "routes/about.tsx"),
   route("feedback", "routes/feedback.tsx"),
   route("privacy", "routes/privacy.tsx"),
+  route("ads.txt", "routes/ads.txt.ts"),
   route("manifest.json", "routes/manifest.json.ts"),
   route("*", "routes/404.tsx"),
 ] satisfies RouteConfig;
